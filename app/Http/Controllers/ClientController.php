3e49919482c8c04d350823f3aa1936f3f7e5d79c
Charles Campista <charlescampista@gmail.com>
2025-09-10 12:19:53 -0300

    Implementando dados do Cliente (Registro, listagem, Exclusão) - Crição de Rotas e Controller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreClientRequest;
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    // Listar clientes
+    public function index(Request $request)
+    {
+        try {
+            $clients = Client::orderBy('id', 'desc')->get();
+
+            return response()->json([
+                'data' => $clients,
+                'count' => $clients->count()
+            ], 200);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => 'Erro ao buscar clientes', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    // Criar cliente
+    public function store(StoreClientRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            $client = Client::create($data);
+
+            return response()->json([
+                'message' => 'Cliente criado com sucesso',
+                'client' => $client
+            ], 201);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => 'Erro ao criar cliente', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    // Delete cliente
+    public function destroy($id)
+    {
+        try {
+            $client = Client::find($id);
+
+            if (!$client) {
+                return response()->json(['message' => 'Cliente não encontrado'], 404);
+            }
+
+            $client->delete();
+
+            return response()->json(['message' => 'Cliente excluído com sucesso'], 200);
+        } catch (\Throwable $e) {
+            return response()->json(['message' => 'Erro ao excluir cliente', 'error' => $e->getMessage()], 500);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientController;
 
 Route::post('auth/register', [AuthController::class, 'register']);
 Route::post('auth/login', [AuthController::class, 'login']);
@@ -9,4 +10,8 @@ Route::post('auth/login', [AuthController::class, 'login']);
 // grupo de rotas protegidas por por autenticação.
 Route::middleware('token.auth')->group(function () {
     Route::post('auth/logout', [AuthController::class, 'logout']);
+
+    Route::get('clients', [ClientController::class, 'index']);
+    Route::post('clients', [ClientController::class, 'store']);
+    Route::delete('clients/{id}', [ClientController::class, 'destroy']);
 });
